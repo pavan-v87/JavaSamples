bb0bc17a91834ffc7c274dfaa26dc6210fbb4f1a
pavan-v87 <ppp1987@gmail.com>
2016-09-02 10:54:23 +0530

    First Commit

---
+++ src/main/java/com/learn/sample/Main.java
@@ -0,0 +1,11 @@
+package com.learn.sample;
+
+/**
+ * Created by pavan.vijayanar on 9/2/2016.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Namaskara!!");
+    }
+}
